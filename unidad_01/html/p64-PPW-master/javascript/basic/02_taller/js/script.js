5d8b792c8ec1cb90545d667c35e9c3d89262fe27
GeorgeGonzalez0 <ggonzalezc3@est.ups.edu.ec>
2024-06-22 12:53:08 -0500

    ok

--- unidad_01/html/p64-PPW-master/javascript/basic/02_taller/js/script.js
+++ unidad_01/html/p64-PPW-master/javascript/basic/02_taller/js/script.js
@@ -2,11 +2,11 @@ var contador = 1
 
 function cargar_elemento() {
     var content = document.getElementById("content")
-    
+    var texto = document.getElementById("texto")
+
     var div = doocument.cargarElement("div")
     div.className = "item-box"
-    var texto = document.createTextNode("Item" + contador)
-    contador++
+    var texto = document.createTextNode( texto.value )
     div.appendChild(texto)
 
     content.appendChild(div)
@@ -15,4 +15,5 @@ function cargar_elemento() {
 function eliminar_elemento(){
     var content = document.getElementById("content")
     content.removeChild(content.lastElementChild)
+
 }
