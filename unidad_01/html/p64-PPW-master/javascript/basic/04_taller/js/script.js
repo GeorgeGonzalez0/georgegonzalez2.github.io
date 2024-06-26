f15cd220b2dc5951bb125ecd59f754e32706afdd
GeorgeGonzalez0 <ggonzalezc3@est.ups.edu.ec>
2024-06-25 21:33:47 -0500

    ok

--- unidad_01/html/p64-PPW-master/javascript/basic/04_taller/js/script.js
+++ unidad_01/html/p64-PPW-master/javascript/basic/04_taller/js/script.js
@@ -1,12 +1,12 @@
-function cargar_elemento() {
-    var content = document.getElementById("Opcion_01")
-    var texto = document.getElementById("Opcion_02")
-    
-    if(opcion == 1) {
+function seleccionar_opcion(opcion) {
+    var opcion_1 = document.getElementById("opcion_01")
+    var opcion_2 = document.getElementById("opcion_02")
+
+    if (opcion == 1) {
         opcion_1.style.zIndex = 2
         opcion_2.style.zIndex = 1
-       } else if (opcion == 2) {
-            opcion_1.style.zIndex = 1
-            opcion_1.style.zIndex = 2
+    } else if (opcion == 2) {
+        opcion_1.style.zIndex = 1
+        opcion_2.style.zIndex = 2
     }
 }
