ccc2bfe899e332600fe4f23f70415ab47cdca2de
GeorgeGonzalez0 <ggonzalezc3@est.ups.edu.ec>
2024-07-20 13:03:53 -0500

    1

--- unidad_01/html/p64-PPW-master/javascript/advanced/ejercicio_01/scripts.js
+++ unidad_01/html/p64-PPW-master/javascript/advanced/ejercicio_01/scripts.js
@@ -9,14 +9,14 @@ function conectar(personaje) {
 }
 
 function obtener_respuesta(){
-    if (xhttp != null && xhttp.readyState == 4) { //readystate: se envio el msj al servidor y el server lo escuchó
+    if (xhttp != null && xhttp.readyState == 4) { //readystate: se envio el msj al servidor y el server lo escuchó//
         if (xhttp.status == 200) {
             let dato = JSON.parse(xhttp.responseText)
             console.log(dato)
             
             let container = document.getElementById('conteiner')
             let personaje = `<div class="container-item"><h1>${dato.name}</h1>
-            <img src="${dato.image}"/>`
+            <img src="${dato.image}"/><div class="container-item">`
             container.innerHTML = personaje
         }
     }
